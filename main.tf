30e4f87b2e2eb438399d70683c1e51a7b9e2ab7a
Soumya Parida <soumyaparida00@gmail.com>
2024-03-06 14:45:23 +0530

    test

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 backend "s3" {
     # Replace this with your bucket name!
-    bucket         = "terraform-vtb"
+    bucket         = "vtb-aws-services"
     key            = "global/s3/terraform.tfstate"
     region         = "us-east-1"
     # Replace this with your DynamoDB table name!
